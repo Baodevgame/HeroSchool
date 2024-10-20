f3e5d1ac5463be6c3ba381290d4657cd03df9d2d
Dev Pham <152831673+Baodevgame@users.noreply.github.com>
2024-10-21 05:16:35 +0700

    Update Registration.cs

--- HeroSchool/Assets/Registration.cs
+++ HeroSchool/Assets/Registration.cs
@@ -1,7 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections;
 
 public class Registration : MonoBehaviour
 {
@@ -13,23 +13,36 @@ public class Registration : MonoBehaviour
     {
         StartCoroutine(Register());
     }
+
     IEnumerator Register()
     {
         WWWForm form = new WWWForm();
         form.AddField("name", nameField.text);
         form.AddField("password", passswordField.text);
-        WWW www = new WWW("http://localhost/sqlconnect/register.php");
-        yield return www;
-        if(www.text=="0")
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/sqlconnect/register.php", form))
         {
-            Debug.Log("User create successfully.");
-            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
-        }
-        else
-        {
-            Debug.Log("User creation false. Error #" + www.text);
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log("User creation failed. Error: " + www.error);
+            }
+            else
+            {
+                if (www.downloadHandler.text == "0")
+                {
+                    Debug.Log("User created successfully.");
+                    UnityEngine.SceneManagement.SceneManager.LoadScene(0); 
+                }
+                else
+                {
+                    Debug.Log("User creation failed. Error: " + www.downloadHandler.text);
+                }
+            }
         }
     }
+
     public void VerifyInputs()
     {
         submitButton.interactable = (nameField.text.Length >= 5 && passswordField.text.Length >= 8);
